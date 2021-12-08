60876a290ac4e4278a5ddf5fda45e8aca6ec7121
AnthonyGorski <tony.gorski13@gmail.com>
2021-12-08 13:41:25 +0100

    debug prod

--- Librairies/autoload.php
+++ Librairies/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    $class = str_replace('\\', '/', $class);
+    $class = str_replace('\\', '/', ucfirst($class));
     var_dump($class);
     require_once "$class.php";
 });
